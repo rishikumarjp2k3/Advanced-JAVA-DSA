1bd381e00166564f9bb84983446edb92359a499b
rishikumarjp2k3 <45505675+rishikumarjp2k3@users.noreply.github.com>
2023-07-17 09:41:01 +0530

    Add files via upload

---
+++ print-pryamids-and-patterns.java
@@ -0,0 +1,66 @@
+int n = 5;                                                                                                         Output
+
+for( int j = 1; j<=n; j++)                   // index 1 to < = n                        * * * * *
+ {                                                       j:  1 2 3 4 5
+     system.out.print(“*”);
+}
+
+for (int j =0; j<n; j++)                     // index 0 to < n                             output 
+ {                                                      j: 0 1 2 3 4                                               * * * * *
+     system.out.print(“*”);
+}
+
+
+int n = 5;                                                                                                         Output
+
+for( int j = 1; j<=n; j++)                   // index 1 to < = n                        * * * * *
+ {                                                       j:  1 2 3 4 5
+     system.out.print(“*”);
+}
+
+for (int j =0; j<n; j++)                     // index 0 to < n                             output 
+ {                                                      j: 0 1 2 3 4                                               * * * * *
+     system.out.print(“*”);
+}
+int = 5                                                                                             * * * * * 
+for (int i =1; i<=n; i++) {                                                            * * * * *
+   for (int j =0; j<n; j++) {                                                            * * * * *
+      system.out.print(“*”);                                                          * * * * *        
+ }                                                                                                       * * * * *
+system.out.println();  
+}
+int = 5                                                                                           i = 1   * 
+for (int i =1; i<=n; i++) {                                                         i = 2   * * 
+   for (int j =1; j<i; j++) {                                                          i = 3  * * * 
+      system.out.print(“*”);                                                        i = 4   * * * *         
+ }                                                                                                     i = 5  * * * * *
+system.out.println();  
+
+int = 5                                                                                          1 to 5   * * * * *
+for (int i =1; i<=n; i++) {                                                         2 to 5   * *  * *
+   for (int j =i; j<=n; j++) {                                                          3 to 5  * * * 
+      system.out.print(“*”);                                                        4 to 5   * *       
+ }                                                                                                     5 to 5  * 
+system.out.println();  
+int = 5                                                                                             
+for (int i =1; i<=n; i++) {                                                            
+   for (int j =i; j<=i; j++) {                                                            
+      system.out.print(“  ”);                                                                 
+ }  
+for (int j =i; j<=n; j++) { 
+     system.out.print(“* ”);   
+}                                                                                              
+system.out.println();  
+}
+int = 5                                                                                             
+for (int i =1; i<=n; i++) {                                                            
+   for (int j =i; j<n; j++) {                                                            
+      system.out.print(“  ”);                                                                 
+ }  
+for (int j =1; j<=i; j++) { 
+     system.out.print(“* ”);   
+}                                                                                              
+system.out.println();  
+}
+
+
